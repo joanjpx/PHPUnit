e605802d7172469cc464e30bd0b22b4907ba60f4
Joan Perez <joansyn.s@gmail.com>
2021-02-17 13:57:30 -0500

    Install PHPUnit as LARAVEL + Tests with Autoload PSR-4 Classes

--- 4/app/Slug.php
+++ 4/app/Slug.php
@@ -6,17 +6,17 @@ class Slug
 {
 	protected $original;
 
-	public function __construct($original)
+	public function __construct(string $original)
 	{
 		$this->original = $original;
 	}
 
-	public function getOriginal()
+	public function getOriginal() : string
 	{
 		return trim($this->original);
 	}
 	
-	public function render()
+	public function render() : string
 	{
 		$slug = str_replace(" ", "-", $this->getOriginal());
 
